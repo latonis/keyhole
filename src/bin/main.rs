2eab9cd24e6b320ca9eda1b940828e8263c88aa7
Jacob Latonis <jlatonis@me.com>
2024-10-28 10:15:19 -0500

    string rep for integer strings and refactor of redis strings

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,9 +6,10 @@ fn main() -> io::Result<()> {
     let mut f = File::open("tests/dump.rdb")?;
     let mut buffer = vec![];
     f.read_to_end(&mut buffer)?;
+
+    let rdb = keyhole::parser::RDB::new(&buffer);
     
-    let rdb_file = keyhole::parser::RDB::parse(&buffer);
-    dbg!(rdb_file);
+    dbg!(rdb);
 
     Ok(())
 }
